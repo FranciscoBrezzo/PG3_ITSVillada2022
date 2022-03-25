97d4c0eb96c8a998d5b886d637e26c00abb5f001
Francisco Brezzo <fran.brezzo@gmail.com>
2022-03-25 08:22:12 -0300

    Resolucion de la guia

---
+++ ES BISIESTO??.py
@@ -0,0 +1,10 @@
+print("Ingrese un año")
+año = int(input())
+
+def EsBisiesto(año):
+    if not año % 4 and (año % 100 or not año % 400):
+        print ("El año " + str(año) + " es un año bisiesto")
+    else:
+        print("El año "+ str(año) + " no es un año bisiesto")
+
+EsBisiesto(año)
