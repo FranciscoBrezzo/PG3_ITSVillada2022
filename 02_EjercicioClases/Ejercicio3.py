e15af49c32444d1c0eca93ceb42c5260c360b852
Francisco Brezzo <fran.brezzo@gmail.com>
2022-04-22 11:19:54 -0300

    resolucion ejercicio 3

--- 02_EjercicioClases/Ejercicio3.py
+++ 02_EjercicioClases/Ejercicio3.py
@@ -6,7 +6,7 @@ class Triangulo:
         self.list = [int(lado1),int(lado2),int(lado3)]
 
     def LadoMayor(self):
-        print(max(self.list))
+        print("El lado mas grande es ",max(self.list))
     
     def EsEquilatero(self):
         if(self.lado1 == self.lado2 and self.lado1 == self.lado3):
@@ -14,8 +14,14 @@ class Triangulo:
         else:
             print("Su triangulo no es equilatero")
 
+print("Ingrese el valor del lado1")
+lado1 = int(input())
+print("Ingrese el valor del lado2")
+lado2 = int(input())
+print("Ingrese el valor del lado3")
+lado3 = int(input())
 
-t1 = Triangulo(5,5,7)
+t1 = Triangulo(lado1,lado2,lado3)
 t1.LadoMayor()
 t1.EsEquilatero()
 
