f3074a12b7e45f98a27544c70d507d5fada565b1
Francisco Brezzo <fran.brezzo@gmail.com>
2022-04-25 08:27:07 -0300

    Resolucion de ejercicio 6

---
+++ 02_EjercicioClases/Ejercicio6.py
@@ -0,0 +1,18 @@
+class Familia:
+    def __init__(self):
+        self.nomP = str(input("Ingrese el nombre del padre:"))
+        self.nomM = str(input("Ingrese el nombre de la madre:"))
+        self.cont = int(input("Ingrese la cantidad de hijos que tiene:"))
+        self.listH = list = []
+        for x in range(self.cont):
+            hijo = input("Ingrese el nombre de los hijos:")
+            self.listH.append(hijo)
+            self.cont += 1
+
+    def __str__(self) -> str:
+        return f"El nombre del padre es: {self.nomP}, el nombre de la madre es: {self.nomM}, y los nombres de los hijos son: {self.listH}"
+
+        
+
+f1 = Familia()
+print(f1)
